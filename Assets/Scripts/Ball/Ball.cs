6cc596147282cfdb4bc5f692c3b12e090ddab3a6
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-08-03 21:28:48 -0700

    Both ball and paddle have been programmed

---
+++ Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody2D rbody;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start() {
+        rbody.velocity = Vector2.up * speed;
+    }
+
+    float WhereToGo(Vector2 ballPos, Vector2 paddlePos){
+        return ballPos.x - paddlePos.x;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Paddle"){
+            float x = WhereToGo(transform.position, other.transform.position);
+            Vector2 dir = new Vector2(x, 1);
+            rbody.velocity = dir * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Bottom"){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");   
+        if(h > 0){
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            transform.position = temp;
+        }
+        if(h<0){
+            Vector3 temp = transform.position;
+            temp.x -= speed * Time.deltaTime;
+            transform.position = temp;
+        }
+    }
+}
