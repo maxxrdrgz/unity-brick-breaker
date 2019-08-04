4e18671859dcc540eaf6c9b210b72eb4a82a5ebc
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-08-03 21:43:24 -0700

    added prefabs added scripts for the bricks base game is pretty much done

---
+++ Assets/Scripts/Brick/BrickBroken.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBroken : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite brokenBrickSprite;
+    private int count;
+
+    private void Start() {
+        count = 0;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Ball"){
+            count++;
+            if(count ==1){
+                GetComponent<SpriteRenderer>().sprite = brokenBrickSprite;
+            }else if(count == 2){
+                Destroy(gameObject);
+            }
+        }    
+    }
+}
