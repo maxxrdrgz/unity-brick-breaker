1d121c15f7c4ab891308bdf0d40954233b959887
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-09-09 20:39:29 -0700

    added comments, minor reformatting

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -12,14 +12,31 @@ public class Ball : MonoBehaviour
     {
         rbody = GetComponent<Rigidbody2D>();
     }
+
+    /** 
+        Moves the gameobjects rigid bodies velocity in the up driection
+    */
     private void Start() {
         rbody.velocity = Vector2.up * speed;
     }
 
+    /** 
+        Returns the difference between the balls x position and the paddles x
+        position.
+
+        @{params} {Vector2} position of the ball
+        @{params} {Vector2} position of the paddle
+    */
     float WhereToGo(Vector2 ballPos, Vector2 paddlePos){
         return ballPos.x - paddlePos.x;
     }
 
+    /** 
+        Detects collision with the paddle. Calculates the x direction the ball
+        should go and changes it's rigid bodies velocity
+
+        @params {Collision2D} The Collision2D data associated with this collision.
+    */
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Paddle"){
             float x = WhereToGo(transform.position, other.transform.position);
@@ -28,6 +45,12 @@ public class Ball : MonoBehaviour
         }
     }
 
+    /** 
+        Detects if the ball has collided with the "Bottom" collider. Bottom
+        being an invisible collider that despawns objects.
+
+        @params {Collider2D} The other Collider2D involved in this collision.
+    */
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Bottom"){
             Destroy(gameObject);
