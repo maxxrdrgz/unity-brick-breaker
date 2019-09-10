7ded2d0637adf4e0a08f41bea0665e3834b421b1
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-09-09 20:43:00 -0700

    commented the brick code

--- Assets/Scripts/Brick/Brick.cs
+++ Assets/Scripts/Brick/Brick.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
+    /** 
+        Detects if the brick gameobject has collided with the ball, if so, the
+        brick gameobject gets destroyed
+
+        @params {Collision2D} The Collision2D data associated with this collision.
+    */
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Ball"){
             DestroyObject(gameObject);
--- Assets/Scripts/Brick/BrickBroken.cs
+++ Assets/Scripts/Brick/BrickBroken.cs
@@ -12,6 +12,14 @@ public class BrickBroken : MonoBehaviour
         count = 0;
     }
     
+    /** 
+        Detects if the brick gameobject has collided with the ball, if done so
+        once (count = 1), the broken brick sprite will replace the current brick
+        sprite. If count = 2, destroy this brick gameobject.
+
+
+        @params {Collision2D} The Collision2D data associated with this collision.
+    */
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Ball"){
             count++;
